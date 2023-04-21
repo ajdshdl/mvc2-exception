5468d00491d1eadb9cc0318f86e6124cb20f937e
kdh <ajdshdl@naver.com>
2023-04-21 15:41:05 +0900

    23-04-21 api exception

--- src/main/java/hello/exception/api/ApiExceptionController.java
+++ src/main/java/hello/exception/api/ApiExceptionController.java
@@ -1,12 +1,16 @@
 package hello.exception.api;
 
+import hello.exception.exception.BadRequestException;
 import hello.exception.exception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @RestController
@@ -26,6 +30,27 @@ public class ApiExceptionController {
 
         return new MemberDto(id, "hello " + id);
     }
+
+    @GetMapping("/api/response-status-ex1")
+    public String responseStatusEx1(){
+        throw new BadRequestException();
+    }
+
+    @GetMapping("/api/response-status-ex2")
+    public String responseStatusEx2(){
+        //@ResponseStatus 는 개발자가 직접 변경할 수 없는 예외에는 적용할 수 없다.- > throw new ResponseStatusException
+        // ResponseStatusExceptionResolver -> sendError( HttpStatus.NOT_FOUND, "error.bad" )
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new IllegalArgumentException());
+    }
+
+    @GetMapping("/api/default-handler-ex")
+    public String defaultException(@RequestParam Integer data) {
+        // DefaultHandlerExceptionResolver 는 스프링 내부에서 발생하는 스프링 예외를 해결한다.
+        return "ok";
+    }
+
+
+
 
     @Data
     @AllArgsConstructor
